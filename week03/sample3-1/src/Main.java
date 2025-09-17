2de172826d6ede7da3dfa10b8164dda258e664a5
yubin086 <ghk4416889@gmail.com>
2025-09-17 17:20:32 +0900

    3주차

---
+++ week03/sample3-1/src/Main.java
@@ -0,0 +1,36 @@
+
+public class Main {
+    public static void main(String[] args) {
+        byte a;
+        short b;
+        int c;
+        long d;
+
+        float f;
+        double k;
+
+        char ch;
+        float PI = 3.141592f;   //symbolic constant(기호적 상수)
+
+        PI = 3.14f;  //3.14는 float형 literal
+
+
+
+        a =(byte) 128;
+        b =(short) 32768;
+        c =(int) 3L;
+        d= 3;
+
+        f = 31.4f;
+        k = 34.1;
+
+        ch = 'A';
+
+        String name = "A";
+        String test = new String( "test");
+
+
+
+
+    }
+}
