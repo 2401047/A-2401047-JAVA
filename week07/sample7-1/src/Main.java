ae3d2eb323a650796a4f9bc0e9cb2d986f4f37b1
yubin086 <ghk4416889@gmail.com>
2025-10-22 15:57:47 +0900

    7주차

---
+++ week07/sample7-1/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int num1, num2;
+
+
+        System.out.print("분자, 분모 입력 : ");
+        num1 = keyboard.nextInt();
+        num2 = keyboard.nextInt();
+
+        float temp = (num2 == 0) ? 0 : 1;
+        String result = temp == 0 ? "ERROE : 분모가 0이면 안됩니다." :
+                String.format("%d / %d = %.2f\n, num1, num2, result");
+
+        if (result == "")  {
+            temp = (float) num1 / num2;
+            System.out.printf("%d / %d = %.2f\n", num1, num2, result);
+        } else {
+            System.err.println(result);
+        }
+
+        if (num2 == 0) {
+
+        }
+
+
+    }
+}
